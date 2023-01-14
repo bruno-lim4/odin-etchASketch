a2cda1d356607475b92f0fb8a9465fd4fec09819
Bruno Lima <brunofigueiredolima@gmail.com>
2023-01-14 14:23:23 -0300

    change color and size finished

--- script.js
+++ script.js
@@ -1,9 +1,17 @@
 const DEFAULT_SIZE = 800;
 
+let colorValue, size;
+
 const board = document.querySelector('.board');
+const colorPicker = document.querySelector('input[type="color"]')
+const sizeRange = document.querySelector('input[type="range"]')
+const labelRange = document.querySelector('.pixel label')
 
 function updateBoard(rows, cols) {
-    board.style.setProperty('--default-size', DEFAULT_SIZE);
+    //deleate board
+    board.innerHTML = '';
+
+    board.style.setProperty('--default-size', DEFAULT_SIZE-1);
     board.style.setProperty('--grid-rows', rows);
     board.style.setProperty('--grid-cols', cols);
 
@@ -11,17 +19,35 @@ function updateBoard(rows, cols) {
         let div = document.createElement('div');
         div.classList.add('unit');
         div.style.border = '1px solid black';
-        div.style.width = `${(DEFAULT_SIZE-(2+2*(rows-1)))/rows}px`;
-        div.style.height = `${(DEFAULT_SIZE-(2+2*(cols-1)))/cols}px`;
+        div.style.width = `${(DEFAULT_SIZE-(2+2*(cols-1)))/cols}px`;
+        div.style.height = `${(DEFAULT_SIZE-(2+2*(rows-1)))/rows}px`;
         board.appendChild(div);
     }
 }
 
 function paintDiv(e) {
     if (!e.target.classList.contains('unit')) return;
-    e.target.style.background = 'blue';
+    e.target.style.background = colorValue;
 }
+
+window.addEventListener('load', () => {
+    colorValue = colorPicker.value;
+    
+    size = +sizeRange.value;
+    labelRange.textContent = `${size} x ${size}`
+
+    updateBoard(size,size);
+})
 
 document.addEventListener('mouseover', paintDiv);
 
-updateBoard(50,50);
+colorPicker.addEventListener('input', (e) => {
+    colorValue = e.target.value;
+})
+
+sizeRange.addEventListener('input', () => {
+    size = +sizeRange.value;
+    labelRange.textContent = `${size} x ${size}`
+
+    updateBoard(size,size);
+})
