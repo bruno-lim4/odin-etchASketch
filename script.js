54e98192bab9c520cdfeae0c523a7d619c1b9dd4
Bruno Lima <brunofigueiredolima@gmail.com>
2023-01-15 12:43:04 -0300

    starting to handle buttons

--- script.js
+++ script.js
@@ -1,11 +1,21 @@
 const DEFAULT_SIZE = 800;
 
-let colorValue, size;
+let colorValue, size, mode = 0;
+
+// mode: 0 - normal
+// mode: 1 - darken
+// mode: 2 - lighten
 
 const board = document.querySelector('.board');
+
 const colorPicker = document.querySelector('input[type="color"]')
 const sizeRange = document.querySelector('input[type="range"]')
+
 const labelRange = document.querySelector('.pixel label')
+
+const darkenMode = document.querySelector('.darken')
+const lightenMode = document.querySelector('.lighten')
+const clearButton = document.querySelector('clear')
 
 function updateBoard(rows, cols) {
     //deleate board
@@ -18,9 +28,9 @@ function updateBoard(rows, cols) {
     for(let i = 0; i < rows*cols; i++) {
         let div = document.createElement('div');
         div.classList.add('unit');
-        div.style.border = '1px solid black';
-        div.style.width = `${(DEFAULT_SIZE-(2+2*(cols-1)))/cols}px`;
-        div.style.height = `${(DEFAULT_SIZE-(2+2*(rows-1)))/rows}px`;
+        // div.style.border = '1px solid black';
+        div.style.width = `${(DEFAULT_SIZE)/cols}px`;
+        div.style.height = `${(DEFAULT_SIZE)/rows}px`;
         board.appendChild(div);
     }
 }
@@ -28,6 +38,13 @@ function updateBoard(rows, cols) {
 function paintDiv(e) {
     if (!e.target.classList.contains('unit')) return;
     e.target.style.background = colorValue;
+}
+
+function handleButtons(e) {
+    if (e.target.classList.contains('darken')) {
+        mode = 1;
+        e.target.classList.add('active');
+    }
 }
 
 window.addEventListener('load', () => {
@@ -50,4 +67,8 @@ sizeRange.addEventListener('input', () => {
     labelRange.textContent = `${size} x ${size}`
 
     updateBoard(size,size);
+})
+
+document.querySelectorAll('.option-button').forEach((btn) => {
+    btn.addEventListener('click', handleButtons)
 })
